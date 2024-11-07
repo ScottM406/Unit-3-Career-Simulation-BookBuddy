a1c9f6bdfd5b9569b112209aaedc8b0949ff8002
Scott <Scottyjmt13@gmail.com>
2024-11-07 09:59:27 -0700

    set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import BookList from "./BookList.jsx";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
 
   return (
     <>
     <h1>Welcome to Book Buddy</h1>
-    <BookList />
+    <Routes>
+    <Route path="/" element={<BookList />} />
+    </Routes>
     </>
   )
 }
