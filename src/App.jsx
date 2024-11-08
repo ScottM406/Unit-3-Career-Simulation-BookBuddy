81c6bd7c19c6d3345b38eba426fd9de7c736cd4c
Scott <Scottyjmt13@gmail.com>
2024-11-08 09:56:07 -0700

    created login page and limited functionality. should work as intended after register funcitonality is created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import BookList from "./BookList.jsx";
 import SingleBook from "./SingleBook.jsx"
+import Login from "./Login.jsx";
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
     <Route path="/" element={<BookList />} />
     <Route path="/:id/:title/:image/:author/:description/:available" element={<SingleBook />} />
+    <Route path="/Login" element={<Login />} />
     </Routes>
     </>
   )
